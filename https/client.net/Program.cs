9273cc90df7fdb9587ba87da04dde96a287370a7
Martin Björkström <martin.bjorkstrom@gmail.com>
2019-10-29 23:39:26 +0200

    Add https sample for grpc-dotnet

---
+++ https/client.net/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greeter.Net;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace client.grpccore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:50051");
+            var client =  new Greeter.Net.Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
