ab5be8e6df86af55b5c1f968177fe0e1fb5948ce
Martin Björkström <martin.bjorkstrom@gmail.com>
2019-10-29 22:29:26 +0200

    Adds Grpc.Core sample for Shared libs.

---
+++ shared-libs/client.grpccore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greeter;
+using Greeter.DTO;
+using Grpc.Core;
+
+namespace client.grpccore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+
+            var client = new GreeterService.GreeterServiceClient(channel);
+            var user = "h4xx0r";
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            await channel.ShutdownAsync();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ shared-libs/server.grpccore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greeter;
+using Greeter.DTO;
+using Grpc.Core;
+
+namespace server.grpccore
+{
+    class GreeterImpl : GreeterService.GreeterServiceBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        static async Task Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { GreeterService.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            await server.ShutdownAsync();
+        }
+    }
+}
